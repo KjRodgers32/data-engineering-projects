a5247506902241451d7e87503dd70485b615099a
KjRodgers32 <kjrodgers26@gmail.com>
2024-04-25 18:18:09 -0400

    configure terraform to add aws s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,30 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
+variable "aws-credentials" {
+  type = string
+}
+
 provider "aws" {
-    region = "us-east-2"
-    shared_credentials_files = ["/Users/Kevin/.aws/config"]
+  region                   = "us-east-2"
+  shared_credentials_files = [var.aws-credentials]
+}
+
+
+resource "aws_s3_bucket" "demo-bucket" {
+  bucket = "kevinr-terraform-demo-bucket"
+
+  force_destroy = true
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
