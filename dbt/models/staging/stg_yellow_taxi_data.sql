241212e188d4ab4eb1261a265d31570673e5b529
Kevin Rodgers Jr <kjrodgers26@gmail.com>
2024-05-26 19:14:52 +0000

    added models for snowflake dimension tables

---
+++ dbt/models/staging/stg_yellow_taxi_data.sql
@@ -0,0 +1,46 @@
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, tpep_pickup_datetime order by vendorid asc) as rn
+  from {{ source('staging','yellow_taxi_data') }}
+  where vendorid is not null 
+)
+select
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
+    vendorid::int as vendorid,
+    ratecodeid::int as ratecodeid,
+    PULOCATIONID::int as pickup_locationid,
+    DOLOCATIONID::int as dropoff_locationid,
+    
+    -- timestamps
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    store_and_fwd_flag,
+    passenger_count::int as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    1 as trip_type,
+
+    -- payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(airport_fee as numeric) as airport_fee,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
+    {{ get_payment_type_description("payment_type") }} as payment_type_description
+from tripdata
+where rn = 1
+
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
